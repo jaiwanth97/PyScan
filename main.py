dde38aa1d8b4b11c5b39499d798c2ad0c0a4b22c
jaiwanth97 <jaiwanth97@gmail.com>
2025-07-30 11:10:26 +0530

    Changes

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
+import typer #type: ignore
 from core.if_ip_given import scan_ports
 from core.scanner import scan_hosts as scan_hosts_lan
 from core.if_cidr_given import scan_hosts
 
-if __name__ == "__main__":
-    while True:
-        print("\n1. Scan the ports (IP required)")
-        print("2. Scan hosts (IP and CIDR required)")
-        print("3. LAN sweep")
-        print("4. Exit")
-        option = input("Select an option: ")
+app = typer.Typer(help="NetHawk - Lightweight Network Scanner")
 
-        if option == "1":
-            ip = input("Enter the IP address: ")
-            scan_ports(ip)
-        elif option == "2":
-            ip = input("Enter the IP address: ")
-            cidr = input("Enter the CIDR: ")
-            scan_hosts(ip,cidr)
-        elif option == "3":
-            print("LAN sweep not implemented yet.")
-            scan_hosts_lan()
-        elif option == "4":
-            print("Exiting...")
-            break
-        else:
-            print("Please select an appropriate option (1-4).")
+@app.command("scan-ports")
+def scan_ports_cli(ip: str = typer.Argument(..., help="IP address to scan ports on")):
+    scan_ports(ip)
+
+@app.command("scan-hosts")
+def scan_hosts_cli(ip: str = typer.Argument(..., help="IP address"),
+                   cidr: int = typer.Argument(..., help="CIDR value (e.g., 24 for /24)")):
+    scan_hosts(ip, cidr)
+
+@app.command("lan-sweep")
+def lan_sweep():
+    scan_hosts_lan()
+
+if __name__ == "__main__":
+    app()
