713f54ba377e61b7b122fdeea2ff609d745bf664
jaiwanth97 <jaiwanth97@gmail.com>
2025-07-30 10:40:20 +0530

    Changes

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from core.if_ip_given import scan_ports
 
 if __name__ == "__main__":
-    ip = input("Enter the IP: ")
-    scan_ports(ip)
+
+    while(True):
+        print("1.Scan the ports (IP required)")
+        print("2.Scan hosts (IP and CIDR required)")
+        print("3.LAN sweep")
+        print("4. Exit")
+        option = input("Select an option: ")
+
+        if option == 1:
+            ip = input("Enter the ip address: ")
+        elif option == 2:
+            ip = input("Enter the ip address: ")
+            cidr = input("Enter the cidr: ")
+        elif option == 3:
+            exit
+        elif option == 4:
+            exit
+        else:
+            print("Please select appropriate option")
